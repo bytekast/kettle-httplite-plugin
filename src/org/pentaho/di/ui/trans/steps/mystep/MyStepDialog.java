5f53b3a825c7858db6cad133816806c084b4636a
Rowell Belen <rowell.belen@bytekast.com>
2014-07-16 01:06:16 -0400

    use md5 hash of step name instead of uiid for re-use

--- src/org/pentaho/di/ui/trans/steps/mystep/MyStepDialog.java
+++ src/org/pentaho/di/ui/trans/steps/mystep/MyStepDialog.java
@@ -1,29 +1,22 @@
 package org.pentaho.di.ui.trans.steps.mystep;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.eclipse.swt.widgets.Shell;
-import org.pentaho.di.core.httplite.dwr.MyStepRemoteProxy;
 import org.pentaho.di.trans.TransMeta;
 import org.pentaho.di.trans.step.BaseStepMeta;
 import org.pentaho.di.trans.step.StepDialogInterface;
 import org.pentaho.di.ui.spoon.Spoon;
 import org.pentaho.di.ui.spoon.dialog.HttpLiteDialog;
 import org.pentaho.di.ui.trans.step.BaseStepDialog;
-import org.springframework.beans.factory.annotation.Autowire;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Configurable;
 
 import java.util.UUID;
 
 /**
  * @author Rowell Belen
  */
-@Configurable(preConstruction = true, autowire = Autowire.BY_TYPE)
 public class MyStepDialog extends BaseStepDialog implements StepDialogInterface {
 
   private final String ID = UUID.randomUUID().toString();
-
-  @Autowired
-  private MyStepRemoteProxy myStepRemoteProxy;
 
   public MyStepDialog(Shell parent, Object in, TransMeta transMeta, String sname) {
     super( parent, (BaseStepMeta) in, transMeta, sname );
@@ -31,20 +24,17 @@ public class MyStepDialog extends BaseStepDialog implements StepDialogInterface 
 
   @Override
   public String open() {
-
-    // Need to register this dialog to the proxy to expose remote methods implemented in IMyStepRemoteProxy
-    if(myStepRemoteProxy != null){
-      myStepRemoteProxy.register(this.ID, new MyStepDialogProxy(this));
-      logBasic("Registered: " + this.ID);
-
-      //TODO need to unregister this when the dialog is closed!!!!!!!
-    }
-
-    final String url = "http://localhost:3388/static/index.html?id=" + this.ID; // pass the unique id to the client
+    new MyStepDialogProxy(this); // register remote proxy listener for web requests
+    final String url = "http://localhost:3388/static/index.html?id=" + getID(); // pass the unique id to the client
     HttpLiteDialog httpLiteDialog = new HttpLiteDialog( Spoon.getInstance().getShell(), "HttpLite", url, "AngularJS Test" );
     httpLiteDialog.open();
 
     return this.stepname;
+  }
+
+  public String getID() {
+    // return ID;
+    return DigestUtils.md5Hex(getStepname());
   }
 
   public String getStepname(){
--- src/org/pentaho/di/ui/trans/steps/mystep/MyStepDialogProxy.java
+++ src/org/pentaho/di/ui/trans/steps/mystep/MyStepDialogProxy.java
@@ -2,21 +2,38 @@ package org.pentaho.di.ui.trans.steps.mystep;
 
 import org.pentaho.di.core.httplite.dwr.IMyStepRemoteProxy;
 import org.pentaho.di.core.httplite.dwr.MyStepRemoteModel;
+import org.pentaho.di.core.httplite.dwr.MyStepRemoteProxy;
+import org.springframework.beans.factory.annotation.Autowire;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
 
 /**
  * @author Rowell Belen
  */
-@Configurable
+@Configurable(autowire = Autowire.BY_TYPE)
 public class MyStepDialogProxy implements IMyStepRemoteProxy{
 
   private MyStepDialog myStepDialog;
 
+  @Autowired
+  private MyStepRemoteProxy myStepRemoteProxy;
+
   public MyStepDialogProxy( MyStepDialog myStepDialog ){
     this.myStepDialog = myStepDialog;
+  }
+
+  @PostConstruct // called after dependencies are injected
+  public void setup(){
+    // Need to register this dialog to the proxy to expose remote methods implemented in IMyStepRemoteProxy
+    if(myStepRemoteProxy != null){
+      myStepRemoteProxy.register(this.myStepDialog.getID(), this);
+
+      //TODO need to unregister this when the dialog is closed!!!!!!!
+    }
   }
 
   @Override
@@ -41,9 +58,5 @@ public class MyStepDialogProxy implements IMyStepRemoteProxy{
   @PreDestroy
   public void destroy(){
     this.myStepDialog = null; // remove reference for garbage collection
-  }
-
-  public MyStepDialog getMyStepDialog() {
-    return myStepDialog;
   }
 }
